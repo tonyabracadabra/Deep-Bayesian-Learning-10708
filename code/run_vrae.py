1b88b5a2840c8ab07e935ea47a9f5076731d0758
Xupeng Tong <tongxupeng.cpu@gmail.com>
2017-03-26 12:09:36 -0400

    change parameter

--- code/run_vrae.py
+++ code/run_vrae.py
@@ -54,8 +54,6 @@ padded_idxes = pad_sequences(idxes, maxlen=time_steps, dtype='int32', \
 
 x_train, x_test = train_test_split(padded_idxes, test_size=0.2, random_state=42)
 
-x_train, x_test = np.zeros((30,time_steps)), np.zeros((10,time_steps))
-
 optimizer, loss, x, y = SimpleSeq2Seq(output_dim=embedding_size, output_length=time_steps, latent_dim=latent_dim, \
                                       batch_size=batch_size, epsilon_std=epsilon_std, lookup_matrix=lookup_matrix, \
                                       input_shape=(time_steps, embedding_size))
